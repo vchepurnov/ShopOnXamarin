196df4ae818245016e677b16c81e026ff1b85030
vchepurnov <chepurnovsteam@bk.ru>
2022-02-20 12:43:43 +0300

    Create ClassLibrary

---
+++ ClassLibrary/Models/ResetPasswordAdminModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity_Server.Models
+{
+    public class ResetPasswordAdminModel
+    {
+        [Required(ErrorMessage = "User is required")]
+        public string Username { get; set; }
+        [Required(ErrorMessage ="New password is required")]
+        public string NewPassword { get; set; }
+        [Required(ErrorMessage = "Confirm new password is required")]
+        public string ConfirmNewPassword { get; set; }
+    }
+}
