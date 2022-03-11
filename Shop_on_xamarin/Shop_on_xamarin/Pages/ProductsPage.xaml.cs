07e3ebfd1d56a07379d9e89e6a29769631e8a5b8
vchepurnov <chepurnovsteam@bk.ru>
2022-03-11 22:41:40 +0300

    Product+

--- Shop_on_xamarin/Shop_on_xamarin/Pages/ProductsPage.xaml.cs
+++ Shop_on_xamarin/Shop_on_xamarin/Pages/ProductsPage.xaml.cs
@@ -7,22 +7,34 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Shop_on_xamarin.ViewModels;
+using System.Windows.Input;
+using Models;
 
 namespace Shop_on_xamarin.Pages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ProductsPage : ContentPage
     {
-        private readonly CategoriesViewModel _vm = new CategoriesViewModel();
+        private readonly ProductListViewModel _vm = new ProductListViewModel();
         public ProductsPage()
         {
             InitializeComponent();
             BindingContext = _vm;
-            //loading();
+            loading();
         }
         public async Task loading()
         {
             await _vm.LoadData();
+        }
+
+        private async void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+           
+        }
+
+        private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            ((ListView)sender).SelectedItem = null;
         }
     }
 }
