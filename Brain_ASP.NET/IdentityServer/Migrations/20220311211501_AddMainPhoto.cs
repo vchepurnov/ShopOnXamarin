3917b511f672f695c569ce657297db03ccbb0c10
Сергей Назаров <nazaroff.serezha2014@gmail.com>
2022-03-12 00:17:14 +0300

    миграция

---
+++ Brain_ASP.NET/IdentityServer/Migrations/20220311211501_AddMainPhoto.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Identity_Server.Migrations
+{
+    public partial class AddMainPhoto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MainPhoto",
+                table: "Product",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MainPhoto",
+                table: "Product");
+        }
+    }
+}
