4ac1a7f680609d2200670d00913b1a47f794936c
Сергей Назаров <nazaroff.serezha2014@gmail.com>
2022-03-13 12:46:37 +0300

    Добавил поле описания для продуктов

---
+++ Brain_ASP.NET/IdentityServer/Migrations/20220313093319_AddProductDescription.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Identity_Server.Migrations
+{
+    public partial class AddProductDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Product",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Product");
+        }
+    }
+}
