26f7b36392c3eb7430faeb50c18873afa0af07db
ILyazZz38 <aigozin2001@mail.ru>
2022-03-18 17:26:28 +0300

    добавил контроллер для столов

---
+++ Brain_ASP.NET/TusbifoodMicroservice/Controllers/SeatsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using TusbifoodMicroservice.Context;
+
+namespace TusbifoodMicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeatsController : ControllerBase
+    {
+        private readonly TusbiFoodContext _context;
+
+        public SeatsController(TusbiFoodContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Seats
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Seat>>> GetSeats()
+        {
+            return await _context.Seats.ToListAsync();
+        }
+
+        // GET: api/Seats/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Seat>> GetSeat(int id)
+        {
+            var seat = await _context.Seats.FindAsync(id);
+
+            if (seat == null)
+            {
+                return NotFound();
+            }
+
+            return seat;
+        }
+
+        // PUT: api/Seats/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSeat(int id, Seat seat)
+        {
+            if (id != seat.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(seat).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SeatExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Seats
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Seat>> PostSeat(Seat seat)
+        {
+            _context.Seats.Add(seat);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSeat", new { id = seat.Id }, seat);
+        }
+
+        // DELETE: api/Seats/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSeat(int id)
+        {
+            var seat = await _context.Seats.FindAsync(id);
+            if (seat == null)
+            {
+                return NotFound();
+            }
+
+            _context.Seats.Remove(seat);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SeatExists(int id)
+        {
+            return _context.Seats.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Brain_ASP.NET/TusbifoodMicroservice/Controllers/TablesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using TusbifoodMicroservice.Context;
+
+namespace TusbifoodMicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TablesController : ControllerBase
+    {
+        private readonly TusbiFoodContext _context;
+
+        public TablesController(TusbiFoodContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Tables
+        [Route("Tables")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Table>>> GetTables()
+        {
+            return await _context.Table.ToListAsync();
+        }
+
+        // GET: api/Tables/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Table>> GetTable(int id)
+        {
+            var table = await _context.Table.FindAsync(id);
+
+            if (table == null)
+            {
+                return NotFound();
+            }
+
+            return table;
+        }
+        [Route("Get-Seats")]
+        [HttpGet]
+        public async Task<List<int>> GetSeats(int tableId) => _context.Seats.Where(e => e.TableId == tableId).Select(a => a.Id).ToList<int>();
+        private bool TableExists(int id)
+        {
+            return _context.Table.Any(e => e.Id == id);
+        }
+    }
+}
